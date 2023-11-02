78860625da2bc239013412ac8f8150ba954532eb
Котельников Владислав <65416898+manulovich@users.noreply.github.com>
2023-11-02 18:51:49 +0300

    Update settings.py

--- zoofriends/zoofriends/settings.py
+++ zoofriends/zoofriends/settings.py
@@ -54,8 +54,8 @@ EMAIL_HOST = 'smtp.yandex.ru'
 EMAIL_PORT = 465
 EMAIL_USE_TLS = False
 EMAIL_USE_SSL = True
-EMAIL_HOST_USER = 'zoofriend.43@yandex.ru'
-EMAIL_HOST_PASSWORD = 'xwkncskjrbhcfmdo'
+EMAIL_HOST_USER = 'email'
+EMAIL_HOST_PASSWORD = 'email password'
 
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 SERVER_EMAIL = EMAIL_HOST_USER
